a50095bfc58653579cad51924b2bcaf776ece9b5
guiguat <guilhermeguatura@sistemagplus.com.br>
2020-11-21 19:20:03 -0300

    test dependency injection

--- app/src/main/java/com/example/mvvmspotifyclone/MainActivity.kt
+++ app/src/main/java/com/example/mvvmspotifyclone/MainActivity.kt
@@ -2,9 +2,17 @@ package com.example.mvvmspotifyclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.RequestManager
 import com.example.mvvmspotifyclone.R.layout.activity_main
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var glide: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main)
