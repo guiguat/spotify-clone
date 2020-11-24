7fa43a9038ad0ef61ef4068ec1508a5e4898e865
guiguat <guilhermeguatura@sistemagplus.com.br>
2020-11-24 00:22:36 -0300

    setting up the music notification

--- app/src/main/java/com/example/mvvmspotifyclone/exoplayer/MusicService.kt
+++ app/src/main/java/com/example/mvvmspotifyclone/exoplayer/MusicService.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
+import com.example.mvvmspotifyclone.exoplayer.callbacks.MusicPlayerNotificationListener
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
