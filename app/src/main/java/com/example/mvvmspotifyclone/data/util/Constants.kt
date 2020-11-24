422ded6c95172dd3cd0a1b634fc11a70242127fe
guiguat <guilhermeguatura@sistemagplus.com.br>
2020-11-24 00:22:36 -0300

    setting up the music notification

--- app/src/main/java/com/example/mvvmspotifyclone/data/util/Constants.kt
+++ app/src/main/java/com/example/mvvmspotifyclone/data/util/Constants.kt
@@ -2,4 +2,6 @@ package com.example.mvvmspotifyclone.data.util
 
 object Constants {
     const val SONG_COLLECTION = "songs"
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
