6dc6f01eceb26a61bea67ae2087c3e3dfa0ba2f4
guiguat <guaturadzn@gmail.com>
2020-11-29 00:39:04 -0300

    create base song adapter

---
+++ app/src/main/java/com/example/mvvmspotifyclone/adapters/SwipeSongAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.mvvmspotifyclone.adapters
+
+import androidx.recyclerview.widget.AsyncListDiffer
+import com.example.mvvmspotifyclone.R
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class SwipeSongAdapter : BaseSongAdapter(R.layout.list_item) {
+    override val differ = AsyncListDiffer(this, diffCallback)
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = songs[position]
+        holder.itemView.apply {
+            val text = "${song.title} - ${song.subtitle}"
+            tvPrimary.text = text
+            setOnClickListener{
+                onItemClickListener?.let { click ->
+                    click(song)
+                }
+            }
+        }
+    }
+}
